34adecbd518ffd904f4003ee2eed17c8a94ebebd
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-11-24 21:48:58 +0700

    Create controller Add.php

---
+++ app/Controllers/Add.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controllers;
+
+class Admin extends BaseController
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'Dashboard Admin | SKYX',
+            'tab' => 'dashboard'
+        ];
+
+        return view('admin/index', $data);
+    }
+
+    public function projects()
+    {
+        $data = [
+            'title' => 'Projects | SKYX',
+            'tab' => 'projects'
+        ];
+
+        return view('admin/projects', $data);
+    }
+
+    public function faqs()
+    {
+        $data = [
+            'title' => 'FAQs | SKYX',
+            'tab' => 'faqs'
+        ];
+
+        return view('admin/faqs', $data);
+    }
+
+    public function users()
+    {
+        $data = [
+            'title' => 'Data Users | SKYX',
+            'tab' => 'users'
+        ];
+
+        return view('admin/datausers', $data);
+    }
+    
+    public function teams()
+    {
+        $data = [
+            'title' => 'Data Teams | SKYX',
+            'tab' => 'teams'
+        ];
+
+        return view('admin/teams', $data);
+    }
+}
