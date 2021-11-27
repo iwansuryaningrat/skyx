8ca0f8cd90940cbd98c080225cb911b94f3ddd64
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-11-27 13:45:09 +0700

    Create Projects Model

---
+++ app/Models/ProjectsModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ProjectsModel extends Model
+{
+    protected $table = 'projects';
+    protected $primarykey = 'id';
+    protected $useTimestamps = true;
+    protected $allowedFields = ['nama', 'deskripsi', 'foto', 'start_date', 'end_date', 'created_at', 'updated_at', 'price', 'bon', 'total_bon', 'total_raised'];
+
+    public function getProject($id)
+    {
+        return $this->where(['id' => $id])->first();
+    }
+}
