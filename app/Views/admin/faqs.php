f977f96ceefedb3a6100ed8e1a7fbed366ecf963
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-11-30 12:42:04 +0700

    Add pavicon

--- app/Views/admin/faqs.php
+++ app/Views/admin/faqs.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<title><?= $title; ?></title>
 	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-	<link rel="icon" href="/admin/assets/img/icon.ico" type="image/x-icon" />
+	<link rel="icon" href="/mainpage/img/favicon.ico" type="image/x-icon" />
 
 	<!-- Fonts and icons -->
 	<script src="/admin/assets/js/plugin/webfont/webfont.min.js"></script>
