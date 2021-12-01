c06fbf15fc82b516d313eb5a9533741e477cd69c
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-12-01 07:49:47 +0700

    Setting Filters

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -63,6 +63,6 @@ class Filters extends BaseConfig
      * @var array
      */
     public $filters = [
-        'login' => ['before' => ['admin']],
+        'login' => ['before' => ['admin/*']],
     ];
 }
