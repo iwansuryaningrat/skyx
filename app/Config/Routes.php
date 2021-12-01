90efae41d61d52bf96b4d4e0de7fa440b031b6f4
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-12-01 08:03:24 +0700

    Fixing filters

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,7 +32,7 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-$routes->get('/admin-page', 'Admin::index');
+$routes->get('/admin-page', 'Admin::index', ['filter' => 'login']);
 
 /*
  * --------------------------------------------------------------------
