c615182842c8e64b060f42432846532a7d8e036c
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-12-06 21:30:49 +0700

    Fixing partner Page

--- app/Views/mainpage/partner.php
+++ app/Views/mainpage/partner.php
@@ -8,7 +8,7 @@
 		<div class="head__other d-flex justify-content-center align-items-center">
 			<div class="header__main">
 				<h1>Partnership</h1>
-				<p class="txt__second">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident amet soluta, est sapiente architecto praesentium necessitatibus placeat nemo.</p>
+				<p class="txt__second">We reach out with leading companies in the crypto industry that have a lot of experience.</p>
 			</div>
 		</div>
 	</div>
@@ -16,9 +16,52 @@
 <!-- end header -->
 
 <!-- partnership section -->
-<div class="sec__x sec__y">
+<div class="sec__x sec__b">
 	<div class="container">
-
+		<div>
+			<div class="partnership">
+				<figure class="partner__icon">
+					<a href="https://universeisland.games/" target="_blank"><img src="/foto/partnership/kommunitas-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="Kommunitas"></a>
+					<div class="partner__cover"></div>
+				</figure>
+				<figure class="partner__icon">
+					<a href="https://www.solchicks.io/" target="_blank"><img src="/foto/partnership/snapex-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="SnapEx"></a>
+					<div class="partner__cover"></div>
+				</figure>
+				<figure class="partner__icon">
+					<a href="https://universeisland.games/" target="_blank"><img src="/foto/partnership/kommunitas-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="Kommunitas"></a>
+					<div class="partner__cover"></div>
+				</figure>
+				<figure class="partner__icon">
+					<a href="https://www.solchicks.io/" target="_blank"><img src="/foto/partnership/snapex-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="SnapEx"></a>
+					<div class="partner__cover"></div>
+				</figure>
+				<figure class="partner__icon">
+					<a href="https://universeisland.games/" target="_blank"><img src="/foto/partnership/kommunitas-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="Kommunitas"></a>
+					<div class="partner__cover"></div>
+				</figure>
+				<figure class="partner__icon">
+					<a href="https://www.solchicks.io/" target="_blank"><img src="/foto/partnership/snapex-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="SnapEx"></a>
+					<div class="partner__cover"></div>
+				</figure>
+				<figure class="partner__icon">
+					<a href="https://universeisland.games/" target="_blank"><img src="/foto/partnership/kommunitas-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="Kommunitas"></a>
+					<div class="partner__cover"></div>
+				</figure>
+				<figure class="partner__icon">
+					<a href="https://www.solchicks.io/" target="_blank"><img src="/foto/partnership/snapex-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="SnapEx"></a>
+					<div class="partner__cover"></div>
+				</figure>
+				<figure class="partner__icon">
+					<a href="https://universeisland.games/" target="_blank"><img src="/foto/partnership/kommunitas-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="Kommunitas"></a>
+					<div class="partner__cover"></div>
+				</figure>
+				<figure class="partner__icon">
+					<a href="https://www.solchicks.io/" target="_blank"><img src="/foto/partnership/snapex-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="SnapEx"></a>
+					<div class="partner__cover"></div>
+				</figure>
+			</div> <!-- end pship -->
+		</div>
 	</div>
 </div>
 <!-- end partnership section -->
