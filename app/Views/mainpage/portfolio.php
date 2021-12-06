14d70c2c5e73d45640151163a17c95f131ed1815
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-12-06 21:27:49 +0700

    Fixing portfolio Page

--- app/Views/mainpage/portfolio.php
+++ app/Views/mainpage/portfolio.php
@@ -8,7 +8,7 @@
 		<div class="head__other d-flex justify-content-center align-items-center">
 			<div class="header__main">
 				<h1>Portfolio</h1>
-				<p class="txt__second">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident amet soluta, est sapiente architecto praesentium necessitatibus placeat nemo.</p>
+				<p class="txt__second">We have many projects that you can participate in and previously also had the best portfolio which we think can help you to manage your finances.</p>
 			</div>
 		</div>
 	</div>
@@ -16,18 +16,39 @@
 <!-- end header -->
 
 <!-- portfolio section -->
-<div class="sec__x sec__y">
+<div class="sec__x sec__b">
 	<div class="container">
 		<div>
 			<div class="portfo">
 				<figure class="portfo__icon">
-					<a href="https://universeisland.games/" target="_blank"><img src="/mainpage/img/partnership/universeisland-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="Universe Island"></a>
+					<a href="https://universeisland.games/" target="_blank"><img src="/foto/portfolio/universeisland-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="Universe Island"></a>
 				</figure>
 				<figure class="portfo__icon">
-					<a href="https://www.solchicks.io/" target="_blank"><img src="/mainpage/img/partnership/solchicks-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="SolChicks"></a>
+					<a href="https://www.solchicks.io/" target="_blank"><img src="/foto/portfolio/solchicks-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="SolChicks"></a>
 				</figure>
 				<figure class="portfo__icon">
-					<a href="https://wingswap.io/" target="_blank"><img src="/mainpage/img/partnership/wingswap-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="WingSwap"></a>
+					<a href="https://wingswap.io/" target="_blank"><img src="/foto/portfolio/wingswap-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="WingSwap"></a>
+				</figure>
+				<figure class="portfo__icon">
+					<a href="https://universeisland.games/" target="_blank"><img src="/foto/portfolio/universeisland-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="Universe Island"></a>
+				</figure>
+				<figure class="portfo__icon">
+					<a href="https://www.solchicks.io/" target="_blank"><img src="/foto/portfolio/solchicks-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="SolChicks"></a>
+				</figure>
+				<figure class="portfo__icon">
+					<a href="https://wingswap.io/" target="_blank"><img src="/foto/portfolio/wingswap-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="WingSwap"></a>
+				</figure>
+				<figure class="portfo__icon">
+					<a href="https://universeisland.games/" target="_blank"><img src="/foto/portfolio/universeisland-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="Universe Island"></a>
+				</figure>
+				<figure class="portfo__icon">
+					<a href="https://www.solchicks.io/" target="_blank"><img src="/foto/portfolio/solchicks-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="SolChicks"></a>
+				</figure>
+				<figure class="portfo__icon">
+					<a href="https://wingswap.io/" target="_blank"><img src="/foto/portfolio/wingswap-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="WingSwap"></a>
+				</figure>
+				<figure class="portfo__icon">
+					<a href="https://wingswap.io/" target="_blank"><img src="/foto/portfolio/wingswap-logo.png" alt="..." class="pship__tt" data-bs-toggle="tooltip" data-bs-placement="top" title="WingSwap"></a>
 				</figure>
 			</div> <!-- end pship -->
 		</div>
