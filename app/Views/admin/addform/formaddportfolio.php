5f13c77cdd11bbe01c68aee1ddc98a0315cba89b
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-12-08 15:55:22 +0700

    Fixing Add Layout

--- app/Views/admin/addform/formaddportfolio.php
+++ app/Views/admin/addform/formaddportfolio.php
@@ -2,6 +2,103 @@
 
 <?= $this->section('add'); ?>
 
-<h1>Hello world</h1>
+<!-- Main Panel -->
+<div class="main-panel">
+    <div class="container">
+        <div class="page-inner">
+            <div class="page-header">
+                <h4 class="page-title">Form Add Partner</h4>
+                <ul class="breadcrumbs">
+                    <li class="nav-home">
+                        <a href="/admin-page">
+                            <i class="flaticon-home"></i>
+                        </a>
+                    </li>
+                    <li class="separator">
+                        <i class="flaticon-right-arrow"></i>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/admin/partnership">Partnership</a>
+                    </li>
+                    <li class="separator">
+                        <i class="flaticon-right-arrow"></i>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/add/formaddpartner">Form Add Partner</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="d-flex align-items-center">
+                                <h4 class="card-title">Add Partner Data</h4>
+                            </div>
+                        </div>
+                        <form id="exampleValidation" action="/add/addpartner" method="POST" enctype="multipart/form-data">
+                            <?= csrf_field(); ?>
+                            <div class="card-body">
+                                <div class="form-group form-show-validation row">
+                                    <label for="name" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Nama <span class="required-label">*</span></label>
+                                    <div class="col-lg-4 col-md-9 col-sm-8">
+                                        <input type="text" class="form-control" id="name" name="name" placeholder="Masukkan Nama Partner" required>
+                                    </div>
+                                </div>
+                                <div class="form-group form-show-validation row">
+                                    <label for="link" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Link <span class="required-label">*</span></label>
+                                    <div class="col-lg-4 col-md-9 col-sm-8">
+                                        <input type="text" class="form-control" placeholder="Masukkan Link Partner" aria-label="link" aria-describedby="link-addon" id="link" name="link" required>
+                                    </div>
+                                </div>
+                                <div class="separator-solid"></div>
+                                <div class="form-group form-show-validation row">
+                                    <label class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Upload Image <span class="required-label">*</span></label>
+                                    <div class="col-lg-4 col-md-9 col-sm-8">
+                                        <div class="input-file input-file-image">
+                                            <img class="img-upload-preview img-circle" width="100" height="100" src="http://placehold.it/100x100" alt="preview">
+                                            <input type="file" class="form-control form-control-file" id="foto" name="foto" accept="image/*" required>
+                                            <label for="foto" class="btn btn-primary btn-round btn-lg"><i class="fa fa-file-image"></i> Upload a Image</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-action">
+                                <div class="row">
+                                    <div class="col-md-12 d-flex justify-content-end">
+                                        <a href="/admin/partnership" class="btn btn-danger" style="border-radius: 100px;">
+                                            Batal
+                                        </a>
+                                        <input class="btn btn-primary ml-4" type="submit" value="Submit" style="border-radius: 100px;">
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="footer">
+        <div class="container-fluid">
+            <nav class="pull-left">
+                <ul class="nav">
+                    <li class="nav-item">
+
+                    </li>
+                    <li class="nav-item">
+
+                    </li>
+                    <li class="nav-item">
+
+                    </li>
+                </ul>
+            </nav>
+            <div class="copyright ml-auto">
+                Skyx © 2021 Copyright. All right reserved
+            </div>
+        </div>
+    </footer>
+</div>
 
 <?= $this->endSection(); ?>
