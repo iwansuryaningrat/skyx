0325a8da94fd191293e4d969fdce489ac3b8b5ca
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2021-12-16 21:27:39 +0700

    Restyling

--- app/Models/VisitorsModel.php
+++ app/Models/VisitorsModel.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Models;
 
@@ -22,18 +22,16 @@ class VisitorsModel extends Model
 
         $todayTime = Time::today('Asia/Jakarta', 'en_US');
         $time = Time::parse($todayTime);
-        $today = $time->getDay();        
+        $today = $time->getDay();
         $sum = 0;
-        foreach ($datavisitors as $visitors) 
-        {
+        foreach ($datavisitors as $visitors) {
             $time = Time::parse($visitors['time']);
             $day = $time->getDay();
-            if ($day == $today)
-            {
+            if ($day == $today) {
                 $sum += $visitors['hits'];
             }
         }
-      
+
         return $sum;
     }
 
@@ -42,51 +40,38 @@ class VisitorsModel extends Model
         $db = \Config\Database::connect();
         $builder = $db->table('visitors');
         $query   = $builder->get();
-        $result  = $query->getResultArray(); 
-        
+        $result  = $query->getResultArray();
+
         $datavisitors = $result;
         $databulan = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        foreach($datavisitors as $visitors) 
-        {
+        foreach ($datavisitors as $visitors) {
             $time = Time::parse($visitors['time']);
             $month = $time->getMonth();
-            if ($month == '1')
-            {
+            if ($month == '1') {
                 $databulan[2] += $visitors['hits'];
-            } else if ($month == '2')
-            {
+            } else if ($month == '2') {
                 $databulan[3] += $visitors['hits'];
-            } else if ($month == '3')
-            {
+            } else if ($month == '3') {
                 $databulan[4] += $visitors['hits'];
-            } else if ($month == '4')
-            {
-                $databulan[5] += $visitors['hits'];                
-            } else if ($month == '5')
-            {
+            } else if ($month == '4') {
+                $databulan[5] += $visitors['hits'];
+            } else if ($month == '5') {
                 $databulan[6] += $visitors['hits'];
-            } else if ($month == '6')
-            {
+            } else if ($month == '6') {
                 $databulan[7] += $visitors['hits'];
-            } else if ($month == '7')
-            {
+            } else if ($month == '7') {
                 $databulan[8] += $visitors['hits'];
-            } else if ($month == '8')
-            {
+            } else if ($month == '8') {
                 $databulan[9] += $visitors['hits'];
-            } else if ($month == '9')
-            {
+            } else if ($month == '9') {
                 $databulan[10] += $visitors['hits'];
-            } else if ($month == '10')
-            {
+            } else if ($month == '10') {
                 $databulan[11] += $visitors['hits'];
-            } else if ($month == '11')
-            {
+            } else if ($month == '11') {
                 $databulan[0] += $visitors['hits'];
-            } else if ($month == '12')
-            {
+            } else if ($month == '12') {
                 $databulan[1] += $visitors['hits'];
-            } 
+            }
         }
 
         return $databulan;
@@ -97,59 +82,46 @@ class VisitorsModel extends Model
         $db = \Config\Database::connect();
         $builder = $db->table('visitors');
         $query   = $builder->get();
-        $result  = $query->getResultArray(); 
-        
+        $result  = $query->getResultArray();
+
         $datavisitors = $result;
 
         $databulan = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
         $databulankumulatif = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-        foreach($datavisitors as $visitors) 
-        {
+        foreach ($datavisitors as $visitors) {
             $time = Time::parse($visitors['time']);
             $month = $time->getMonth();
-            if ($month == '1')
-            {
+            if ($month == '1') {
                 $databulan[2] += $visitors['hits'];
-            } else if ($month == '2')
-            {
+            } else if ($month == '2') {
                 $databulan[3] += $visitors['hits'];
-            } else if ($month == '3')
-            {
+            } else if ($month == '3') {
                 $databulan[4] += $visitors['hits'];
-            } else if ($month == '4')
-            {
-                $databulan[5] += $visitors['hits'];                
-            } else if ($month == '5')
-            {
+            } else if ($month == '4') {
+                $databulan[5] += $visitors['hits'];
+            } else if ($month == '5') {
                 $databulan[6] += $visitors['hits'];
-            } else if ($month == '6')
-            {
+            } else if ($month == '6') {
                 $databulan[7] += $visitors['hits'];
-            } else if ($month == '7')
-            {
+            } else if ($month == '7') {
                 $databulan[8] += $visitors['hits'];
-            } else if ($month == '8')
-            {
+            } else if ($month == '8') {
                 $databulan[9] += $visitors['hits'];
-            } else if ($month == '9')
-            {
+            } else if ($month == '9') {
                 $databulan[10] += $visitors['hits'];
-            } else if ($month == '10')
-            {
+            } else if ($month == '10') {
                 $databulan[11] += $visitors['hits'];
-            } else if ($month == '11')
-            {
+            } else if ($month == '11') {
                 $databulan[0] += $visitors['hits'];
-            } else if ($month == '12')
-            {
+            } else if ($month == '12') {
                 $databulan[1] += $visitors['hits'];
-            } 
+            }
         }
 
-        for ($i=0; $i < 12; $i++) { 
-            for ($j=0; $j <= $i; $j++) { 
-                $databulankumulatif[$i] += $databulan[$j]; 
+        for ($i = 0; $i < 12; $i++) {
+            for ($j = 0; $j <= $i; $j++) {
+                $databulankumulatif[$i] += $databulan[$j];
             }
         }
 
